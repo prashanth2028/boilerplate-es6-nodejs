0a7081b240618909d5d160fb06eb342d34fe9f76
prashanth2028 <prashanth@sparkouttech.com>
2024-10-15 17:43:42 +0530

    error handeler in router

--- source/index.js
+++ source/index.js
@@ -1,11 +1,21 @@
 import dotenv from 'dotenv';
 import app from './routes/route';
 import { connectDB } from './config/DB_connection';
+import { converter , notFound , handler} from './utlis/errorHandler';
 
 // load .env variables into process.env
 dotenv.config();
 
 connectDB();
+
+// if error is not an instanceOf APIError, convert it.
+app.use(converter);
+
+// catch 404 and forward to error handler
+app.use(notFound);
+
+// error handler, send stacktrace only during development
+app.use(handler);
 
 app.listen(process.env.PORT, () => {
     console.log(`prot is running on ${process.env.PORT}`);
