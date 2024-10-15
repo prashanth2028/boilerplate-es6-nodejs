f4e493f8be8418f0c230614d1b273af893e5fab2
prashanth2028 <prashanth@sparkouttech.com>
2024-10-15 11:44:45 +0530

    create express file and import in main file

--- source/index.js
+++ source/index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+import dotenv from 'dotenv';
+import app from './config/express';
+
+// load .env variables into process.env
+dotenv.config();
+
+
+app.listen(process.env.PORT, () => {
+    console.log(`prot is running on ${process.env.PORT}`);
+});
